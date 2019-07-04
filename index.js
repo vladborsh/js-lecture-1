d2df36d0e1238465ed37da613dbc18e5ca4983bf
vlad.borsh <vladborsh95s322@gmail.com>
2019-07-04 20:08:36 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer(function (request, response) {
+  console.log(request.method, request.url);
+
+  if (request.url == '/') {
+    const content = fs.readFileSync('index.html', 'utf8');
+    response.end(content);
+  } else if (request.url == '/style.css') {
+    const content = fs.readFileSync('style.css', 'utf8');
+    response.end(content);
+  }
+})
+
+server.listen(3000);
+console.log('server started!!');
